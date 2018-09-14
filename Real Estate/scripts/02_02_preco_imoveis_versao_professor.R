d1713558345cec97992917c00461c9c7b2d515ff
RicardoPedrotti <rpedrottivf@gmail.com>
2018-09-14 15:52:42 -0300

    pós aula 5

--- Real Estate/scripts/02_02_preco_imoveis_versao_professor.R
+++ Real Estate/scripts/02_02_preco_imoveis_versao_professor.R
@@ -2,26 +2,28 @@
 # Script para criacao do modelo e avaliacao
 #
 
-load("../data/20140917_imoveis_completo_tratado.rda")
+load("/data/20140917_imoveis_completo_tratado.rda")
 sort(table(imoveis$bairro), decreasing = TRUE)
 length(table(imoveis$bairro))
 
-# separando o dataset de treino com o de teste (aleatóriamente, pq n sao dados categóricos)
+# separando o dataset de treino com o de teste (aleat?riamente, pq n sao dados categ?ricos)
 set.seed(1234)
 ind <- sample(2, nrow(imoveis), replace = TRUE, prob = c(0.8, 0.2))
+#a funÃ§Ã£o sample acima faz um sorteio entre 1 e 2 para criar indices para dividir o dataset entre
 train <- imoveis[ind == 1, ]
 test <- imoveis[ind == 2,]
 
-model <- lm(preco ~ ., data=train) #traz a função para aprender uma regressão linear (quero saber preço em relação a todos os outros atributos do dataset (.))
-summary(model) #Residuals é o erro
-plot(model)
+model <- lm(preco ~ ., data=train) #traz a fun??o para aprender uma regress?o linear (quero saber pre?o em rela??o a todos os outros atributos do dataset (.))
+model
+summary(model) #Residuals ? o erro
+plot(model) #o plot para modelos de regrassÃ£o imprime 4 grÃ¡ficos. O primeiro Ã© o mais importante
 
 
 # Function that returns Root Mean Squared Error
-rmse <- function(error)
+rmse <- function(error)#declara que Ã© uma funÃ§Ã£o
 {
   sqrt(mean(error^2))
-}
+} #diferenÃ§a entre o que o modelo fornece e o verdadeiro (estima qual modelo erra menos de forma grosseira)
 
 # Function that returns Mean Absolute Error
 mae <- function(error)
@@ -29,14 +31,16 @@ mae <- function(error)
   mean(abs(error))
 }
 
-test$pred <- predict(model, test)
-rmse(test$preco - test$pred) #
-mae(test$preco - test$pred) #diferença absoluta entre o erro e tira uma média
+test$pred <- predict(model, test) #cria uma nova coluna chamada pred na tabela test e coloca o predict
+rmse(test$preco - test$pred) # Ã© a funÃ§Ã£o que criamos lÃ¡ em cima para ver o quao bom o modelo tÃ¡ (mais prÃ³ximo de zero Ã© melhor)
+mae(test$preco - test$pred) #diferen?a absoluta entre o erro e tira uma m?dia
 
 test$error <- abs(test$preco - test$pred) #cria coluna de erro
 View(test)
 
-#vamos descobrir se ele tá over fittando ou não (usando o conjunto de treinamento)
+#vamos descobrir se ele t? over fittando ou n?o (usando o conjunto de treinamento)
 predTrain <- predict(model, train)
 rmse(train$preco - predTrain)
 mae(train$preco - predTrain)
+
+
